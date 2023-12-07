72c54f7051fddd42395e4e991daf61a7438552c2
erool2016 <112575876+erool2016@users.noreply.github.com>
2023-12-07 16:41:44 +0300

    Add files via upload

---
+++ 1.1-first-project/first_project/app/views.py
@@ -0,0 +1,38 @@
+import os
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from datetime import datetime
+
+def home_view(request):
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir'),
+    }
+    
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    # обратите внимание – здесь HTML шаблона нет, 
+    # возвращается просто текст
+    current_time = datetime.now()
+    print(current_time)
+    msg = f'Текущее время: {current_time.strftime("%H:%M:%S")}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    files = os.listdir(os.getcwd())
+    print(files)
+    return HttpResponse(f'{files}')
+
