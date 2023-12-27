194b57c28cbd91cd69717a1f3071993cb021e557
erool2016 <112575876+erool2016@users.noreply.github.com>
2023-12-27 12:40:13 +0300

    Add files via upload

--- phones/views.py
+++ phones/views.py
@@ -25,5 +25,8 @@ def show_catalog(request):
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+
+    phone = Phone.objects.filter(slug=slug)[0]
+    context = {'phone': phone}
+
     return render(request, template, context)
